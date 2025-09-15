a2b32f42cbfa58ab3264e5188247cbd2ae2f80c6
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-09-15 15:49:58 -0400

    split favorite drivers and teams

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -24,15 +24,14 @@ CREATE TABLE teams(
   team_color text NOT NULL
 );
 
-CREATE TABLE user_favorites (
+CREATE TABLE favorite_drivers (
   user_id integer REFERENCES users(id) ON DELETE CASCADE,
   driver_id integer REFERENCES drivers(driver_number) ON DELETE CASCADE,
+  PRIMARY KEY (user_id, driver_id)
+);
+
+CREATE TABLE favorite_teams (
+  user_id integer REFERENCES users(id) ON DELETE CASCADE,
   team_id integer REFERENCES teams(id) ON DELETE CASCADE,
-  CONSTRAINT unique_user_driver UNIQUE(user_id, driver_id),
-  CONSTRAINT unique_user_team UNIQUE(user_id, team_id),
-  -- Optionally, enforce only one favorite per row:
-  CONSTRAINT only_one_favorite CHECK (
-    (driver_id IS NOT NULL AND team_id IS NULL) OR
-    (driver_id IS NULL AND team_id IS NOT NULL)
-  )
+  PRIMARY KEY (user_id, team_id)
 );
