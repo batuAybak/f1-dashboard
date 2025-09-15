237282c025b24081c6586b157682711b4c3e4d8d
Alvaro Carvajal Roldan <you@example.comacarvajalrol@hotmail.com>
2025-09-15 12:58:48 -0700

    userFavorites query

--- backend/db/queries/userFavorites.js
+++ backend/db/queries/userFavorites.js
@@ -2,7 +2,7 @@ import db from "#db/client";
 
 export async function userFavoriteDriver(userId, driverId) {
   const SQL = `
-    INSERT INTO user_favorites (user_id, driver_id)
+    INSERT INTO favorite_drivers (user_id, driver_id)
     VALUES ($1, $2)
     RETURNING *
     `;
@@ -14,7 +14,7 @@ export async function userFavoriteDriver(userId, driverId) {
 
 export async function userFavoriteTeam(userId, teamId) {
   const SQL = `
-    INSERT INTO user_favorites (user_id, team_id)
+    INSERT INTO favorite_teams (user_id, team_id)
     VALUES ($1, $2)
     RETURNING *
     `;
@@ -22,4 +22,28 @@ export async function userFavoriteTeam(userId, teamId) {
     rows: [favorite],
   } = await db.query(SQL, [userId, teamId]);
   return favorite;
+}
+
+export async function deleteUserFavoriteDriver(userId, driverId) {
+  const SQL = `
+    DELETE FROM favorite_drivers
+    WHERE user_id = $1 AND driver_id = $2
+    RETURNING *
+    `;
+  const {
+    rows: [favoriteDriver],
+  } = await db.query(SQL, [userId, driverId]);
+  return favoriteDriver;
+}
+
+export async function deleteUserFavoriteTeam(userId, teamId) {
+  const SQL = `
+    DELETE FROM favorite_teams
+    WHERE user_id = $1 AND team_id = $2
+    RETURNING *
+    `;
+  const {
+    rows: [favoriteTeam],
+  } = await db.query(SQL, [userId, teamId]);
+  return favoriteTeam;
 }
