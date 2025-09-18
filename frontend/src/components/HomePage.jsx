2f1094981ba162fb663d814413daec4defaeecb2
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-09-18 11:21:21 -0400

    add plain home page

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,3 +1,35 @@
+import useQuery from "../api/useQuery";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const {
+    data: calendar,
+    loadingCalendar,
+    errorCalendar,
+  } = useQuery("/calendar", "calendar");
+  if (loadingCalendar) return <p>Loading...</p>;
+  if (errorCalendar)
+    return <p>Error loading calendar: {errorCalendar.message}</p>;
+  if (!calendar) return <p>No calendar data found.</p>;
+
+  //Find the closest upcoming race from the calendar data
+  const closestRace = calendar.find(
+    (race) => new Date(race.date_start) > new Date()
+  );
+  console.log(closestRace);
+  //TODO WORKING ON HOME PAGE COUNTDOWN PAGE
+  return (
+    <>
+      <h1>Welcome to the F1 Dashboard</h1>
+      <p>Here you can find information about drivers, teams, and races.</p>
+      <p>Add your favorite teams or drivers to get personalized updates.</p>
+      <p>Next Race:</p>
+      {closestRace && (
+        <p>
+          {closestRace.meeting_official_name}
+          <br />
+          {new Date(closestRace.date_start).toLocaleDateString()}
+        </p>
+      )}
+    </>
+  );
 }
