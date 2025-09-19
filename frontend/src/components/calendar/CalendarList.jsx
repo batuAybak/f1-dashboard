5a4c6df9563246d2756a773bf2f52499d52535f2
Alvaro Carvajal Roldan <you@example.comacarvajalrol@hotmail.com>
2025-09-19 09:43:38 -0700

    calendarPage css

--- frontend/src/components/calendar/CalendarList.jsx
+++ frontend/src/components/calendar/CalendarList.jsx
@@ -3,10 +3,18 @@ import { useNavigate } from "react-router";
 export default function CalendarList({ circuit }) {
   const navigate = useNavigate();
 
+  console.log(circuit.image);
+
   return (
     <li
       className="calendar-card"
       onClick={() => navigate(`/calendar/${circuit.circuit_short_name}`)}
+      style={{
+        backgroundImage: `url(${circuit.image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <h2 className="calendar-grandPrix">{circuit?.meeting_official_name}</h2>
     </li>
