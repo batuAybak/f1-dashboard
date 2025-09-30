55fabaa8d073611a3aa657f9b831cecd045b45b3
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-09-30 16:12:03 -0400

    Add theme support to ProfilePage component
    Integrated theme context into ProfilePage to dynamically adjust button styles and pass theme props to child components. Updated structure and headings for improved clarity and user experience.

--- frontend/src/components/profile/ProfilePage.jsx
+++ frontend/src/components/profile/ProfilePage.jsx
@@ -4,10 +4,12 @@ import DriverList from "../drivers/DriverList.jsx";
 import TeamList from "../teams/TeamList.jsx";
 import AddFavoriteDriverSection from "./AddFavoriteDriverSection.jsx";
 import AddFavoriteTeamSection from "./AddFavoriteTeamSection.jsx";
+import { useTheme } from "../ThemeContext.jsx";
 
 export default function ProfilePage() {
   const { data: user, loading, error } = useQuery("/users/profile", "profile"); // Fetch user profile with favorite driver and team
-
+  const { theme } = useTheme();
+  const classTheme = theme === "light" ? "dark" : "light";
   const userFavoriteDriver = user?.userFavoriteDriver;
   const userFavoriteTeam = user?.userFavoriteTeam;
 
@@ -32,53 +34,56 @@ export default function ProfilePage() {
   if (error) return <p>Error! {error}</p>;
 
   return (
-    <section className="profile-main">
-      <div>
-        <h1 className="welcome-message">
+    <div className="profile-main">
+      <div className="profile-header">
+        <h2 className="welcome-message">
           Welcome, {user?.user.first_name} {user?.user.last_name}!
-        </h1>
+        </h2>
+        <p>Use the sections below to manage your favorite driver and team.</p>
       </div>
 
-      {/* ----- Favorite Driver ----- */}
-      <div className="favorite-driver">
-        <h2>Favorite Driver</h2>
-        {userFavoriteDriver === undefined ? (
-          <AddFavoriteDriverSection />
-        ) : (
-          <>
-            <DriverList
-              driver={userFavoriteDriver}
-              teamName={userFavoriteDriver.team_name}
-            />
-            <br />
-            <button
-              className="btn btn-outline-dark btn-sm"
-              onClick={() => removeDriver()}
-            >
-              Remove Favorite Driver
-            </button>
-          </>
-        )}
-      </div>
+      <div className="favorites">
+        {/* ----- Favorite Driver ----- */}
+        <section className="favorite-driver">
+          <h3>Favorite Driver</h3>
+          {userFavoriteDriver === undefined ? (
+            <AddFavoriteDriverSection classTheme={classTheme} theme={theme} />
+          ) : (
+            <>
+              <DriverList
+                driver={userFavoriteDriver}
+                teamName={userFavoriteDriver.team_name}
+              />
+              <br />
+              <button
+                className={`btn btn-outline-${classTheme} btn-sm`}
+                onClick={() => removeDriver()}
+              >
+                Remove Favorite Driver
+              </button>
+            </>
+          )}
+        </section>
 
-      {/* ----- Favorite Team ----- */}
-      <div className="favorite-team">
-        <h2>Favorite Team</h2>
-        {userFavoriteTeam === undefined ? (
-          <AddFavoriteTeamSection />
-        ) : (
-          <>
-            <TeamList team={userFavoriteTeam} />
-            <br />
-            <button
-              className="btn btn-outline-dark btn-sm"
-              onClick={() => removeTeam()}
-            >
-              Remove Favorite Team
-            </button>
-          </>
-        )}
+        {/* ----- Favorite Team ----- */}
+        <section className="favorite-team">
+          <h3>Favorite Team</h3>
+          {userFavoriteTeam === undefined ? (
+            <AddFavoriteTeamSection classTheme={classTheme} theme={theme} />
+          ) : (
+            <>
+              <TeamList team={userFavoriteTeam} />
+              <br />
+              <button
+                className={`btn btn-outline-${classTheme} btn-sm`}
+                onClick={() => removeTeam()}
+              >
+                Remove Favorite Team
+              </button>
+            </>
+          )}
+        </section>
       </div>
-    </section>
+    </div>
   );
 }
