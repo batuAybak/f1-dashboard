0fac64bb8997cb64f05d1c83c4283e12246adcab
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2026-03-27 15:15:28 -0400

    Add drivers seed data (drivers.js)
    Create backend/db/data/drivers.js exporting a drivers array with 22 driver records. Each record includes metadata used for seeding or lookup (driver_number, broadcast_name, full_name, name_acronym, team_name, team_color, first_name, last_name, headshot_url, country_code). File also contains inline comments about avatar/image URL transforms and sizing.

---
+++ backend/db/data/drivers.js
@@ -0,0 +1,276 @@
+// "c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png" for avatar size 
+export const drivers = [
+    {
+        "driver_number": 1,
+        "broadcast_name": "L NORRIS",
+        "full_name": "Lando NORRIS",
+        "name_acronym": "NOR",
+        "team_name": "McLaren",
+        "team_color": "F47600",
+        "first_name": "Lando",
+        "last_name": "Norris",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png.transform/1col/image.png",
+        "country_code": "GBR"
+    },
+    {
+        "driver_number": 3,
+        "broadcast_name": "M VERSTAPPEN",
+        "full_name": "Max VERSTAPPEN",
+        "name_acronym": "VER",
+        "team_name": "Red Bull Racing",
+        "team_color": "4781D7",
+        "first_name": "Max",
+        "last_name": "Verstappen",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/redbullracing/maxver01/2026redbullracingmaxver01right.webp",
+        "country_code": "NED"
+    },
+    {
+        "driver_number": 5,
+        "broadcast_name": "G BORTOLETO",
+        "full_name": "Gabriel BORTOLETO",
+        "name_acronym": "BOR",
+        "team_name": "Audi",
+        "team_color": "F50537",
+        "first_name": "Gabriel",
+        "last_name": "Bortoleto",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/audi/gabbor01/2026audigabbor01right.webp",
+        "country_code": "BRA"
+    },
+    {
+        "driver_number": 6,
+        "broadcast_name": "I HADJAR",
+        "full_name": "Isack HADJAR",
+        "name_acronym": "HAD",
+        "team_name": "Red Bull Racing",
+        "team_color": "4781D7",
+        "first_name": "Isack",
+        "last_name": "Hadjar",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/I/ISAHAD01_Isack_Hadjar/isahad01.png.transform/1col/image.png",
+        "country_code": "FRA"
+    },
+    {
+        "driver_number": 10,
+        "broadcast_name": "P GASLY",
+        "full_name": "Pierre GASLY",
+        "name_acronym": "GAS",
+        "team_name": "Alpine",
+        "team_color": "00A1E8",
+        "first_name": "Pierre",
+        "last_name": "Gasly",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/P/PIEGAS01_Pierre_Gasly/piegas01.png.transform/1col/image.png",
+        "country_code": "FRA"
+    },
+    {
+        "driver_number": 11,
+        "broadcast_name": "S PEREZ",
+        "full_name": "Sergio PEREZ",
+        "name_acronym": "PER",
+        "team_name": "Cadillac",
+        "team_color": "909090",
+        "first_name": "Sergio",
+        "last_name": "Perez",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/cadillac/serper01/2026cadillacserper01right.webp",
+        "country_code": "MEX"
+    },
+    {
+        "driver_number": 12,
+        "broadcast_name": "K ANTONELLI",
+        "full_name": "Kimi ANTONELLI",
+        "name_acronym": "ANT",
+        "team_name": "Mercedes",
+        "team_color": "00D7B6",
+        "first_name": "Kimi",
+        "last_name": "Antonelli",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/K/ANDANT01_Kimi_Antonelli/andant01.png.transform/1col/image.png",
+        "country_code": "ITA"
+    },
+    {
+        "driver_number": 14,
+        "broadcast_name": "F ALONSO",
+        "full_name": "Fernando ALONSO",
+        "name_acronym": "ALO",
+        "team_name": "Aston Martin",
+        "team_color": "229971",
+        "first_name": "Fernando",
+        "last_name": "Alonso",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png.transform/1col/image.png",
+        "country_code": "ESP"
+    },
+    {
+        "driver_number": 16,
+        "broadcast_name": "C LECLERC",
+        "full_name": "Charles LECLERC",
+        "name_acronym": "LEC",
+        "team_name": "Ferrari",
+        "team_color": "ED1131",
+        "first_name": "Charles",
+        "last_name": "Leclerc",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/1col/image.png",
+        "country_code": "MON"
+    },
+    {
+        "driver_number": 18,
+        "broadcast_name": "L STROLL",
+        "full_name": "Lance STROLL",
+        "name_acronym": "STR",
+        "team_name": "Aston Martin",
+        "team_color": "229971",
+        "first_name": "Lance",
+        "last_name": "Stroll",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png.transform/1col/image.png",
+        "country_code": "CAN"
+    },
+    {
+        "driver_number": 23,
+        "broadcast_name": "A ALBON",
+        "full_name": "Alexander ALBON",
+        "name_acronym": "ALB",
+        "team_name": "Williams",
+        "team_color": "1868DB",
+        "first_name": "Alexander",
+        "last_name": "Albon",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png.transform/1col/image.png",
+        "country_code": "THA"
+    },
+    {
+        "driver_number": 27,
+        "broadcast_name": "N HULKENBERG",
+        "full_name": "Nico HULKENBERG",
+        "name_acronym": "HUL",
+        "team_name": "Audi",
+        "team_color": "F50537",
+        "first_name": "Nico",
+        "last_name": "Hulkenberg",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/audi/nichul01/2026audinichul01right.webp",
+        "country_code": "GER"
+    },
+    {
+        "driver_number": 30,
+        "broadcast_name": "L LAWSON",
+        "full_name": "Liam LAWSON",
+        "name_acronym": "LAW",
+        "team_name": "Racing Bulls",
+        "team_color": "6C98FF",
+        "first_name": "Liam",
+        "last_name": "Lawson",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LIALAW01_Liam_Lawson/lialaw01.png.transform/1col/image.png",
+        "country_code": "NZL"
+    },
+    {
+        "driver_number": 31,
+        "broadcast_name": "E OCON",
+        "full_name": "Esteban OCON",
+        "name_acronym": "OCO",
+        "team_name": "Haas F1 Team",
+        "team_color": "9C9FA2",
+        "first_name": "Esteban",
+        "last_name": "Ocon",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/E/ESTOCO01_Esteban_Ocon/estoco01.png.transform/1col/image.png",
+        "country_code": "FRA"
+    },
+    {
+        "driver_number": 41,
+        "broadcast_name": "A LINDBLAD",
+        "full_name": "Arvid LINDBLAD",
+        "name_acronym": "LIN",
+        "team_name": "Racing Bulls",
+        "team_color": "6C98FF",
+        "first_name": "Arvid",
+        "last_name": "Lindblad",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/racingbulls/arvlin01/2026racingbullsarvlin01right.webp",
+        "country_code": "GBR"
+    },
+    {
+        "driver_number": 43,
+        "broadcast_name": "F COLAPINTO",
+        "full_name": "Franco COLAPINTO",
+        "name_acronym": "COL",
+        "team_name": "Alpine",
+        "team_color": "00A1E8",
+        "first_name": "Franco",
+        "last_name": "Colapinto",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FRACOL01_Franco_Colapinto/fracol01.png.transform/1col/image.png",
+        "country_code": "ARG"
+    },
+    {
+        "driver_number": 44,
+        "broadcast_name": "L HAMILTON",
+        "full_name": "Lewis HAMILTON",
+        "name_acronym": "HAM",
+        "team_name": "Ferrari",
+        "team_color": "ED1131",
+        "first_name": "Lewis",
+        "last_name": "Hamilton",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/ferrari/lewham01/2026ferrarilewham01right.webp",
+        "country_code": "GBR"
+    },
+    {
+        "driver_number": 55,
+        "broadcast_name": "C SAINZ",
+        "full_name": "Carlos SAINZ",
+        "name_acronym": "SAI",
+        "team_name": "Williams",
+        "team_color": "1868DB",
+        "first_name": "Carlos",
+        "last_name": "Sainz",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png.transform/1col/image.png",
+        "country_code": "ESP"
+    },
+    {
+        "driver_number": 63,
+        "broadcast_name": "G RUSSELL",
+        "full_name": "George RUSSELL",
+        "name_acronym": "RUS",
+        "team_name": "Mercedes",
+        "team_color": "00D7B6",
+        "first_name": "George",
+        "last_name": "Russell",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png.transform/1col/image.png",
+        "country_code": "GBR"
+    },
+    {
+        "driver_number": 77,
+        "broadcast_name": "V BOTTAS",
+        "full_name": "Valtteri BOTTAS",
+        "name_acronym": "BOT",
+        "team_name": "Cadillac",
+        "team_color": "909090",
+        "first_name": "Valtteri",
+        "last_name": "Bottas",
+        "headshot_url": "https://media.formula1.com/image/upload/c_thumb,z_0.5,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/cadillac/valbot01/2026cadillacvalbot01right.webp",
+        "country_code": "FIN"
+    },
+    {
+        "driver_number": 81,
+        "broadcast_name": "O PIASTRI",
+        "full_name": "Oscar PIASTRI",
+        "name_acronym": "PIA",
+        "team_name": "McLaren",
+        "team_color": "F47600",
+        "first_name": "Oscar",
+        "last_name": "Piastri",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png.transform/1col/image.png",
+        "country_code": "AUS"
+    },
+    {
+        "driver_number": 87,
+        "broadcast_name": "O BEARMAN",
+        "full_name": "Oliver BEARMAN",
+        "name_acronym": "BEA",
+        "team_name": "Haas F1 Team",
+        "team_color": "9C9FA2",
+        "first_name": "Oliver",
+        "last_name": "Bearman",
+        "headshot_url": "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OLIBEA01_Oliver_Bearman/olibea01.png.transform/1col/image.png",
+        "country_code": "GBR"
+    }
+]
+
+// 1-full
+// 2-avatar
+// 3-mine
+// https://media.formula1.com/image/upload/c_fill,w_720/q_auto/v1740000001/common/f1/2026/racingbulls/arvlin01/2026racingbullsarvlin01right.webp
+// https://media.formula1.com/image/upload/c_lfill,w_64/q_auto/d_common:f1:2026:fallback:driver:2026fallbackdriverright.webp/v1740000001/common/f1/2026/racingbulls/arvlin01/2026racingbullsarvlin01right.webp
+// https://media.formula1.com/image/upload/c_fill,g_face,w_150,h_150/q_auto/f_png/v1740000000/common/f1/2025/alpine/fracol01/2025alpinefracol01right.webp
+
+// https://media.formula1.com/image/upload/c_fill,g_face,w_150,h_150/q_auto/f_png/v1740000001/common/f1/2026/racingbulls/arvlin01/2026racingbullsarvlin01right.webp
